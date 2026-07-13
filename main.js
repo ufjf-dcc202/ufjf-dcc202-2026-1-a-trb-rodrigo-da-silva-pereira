9a2e99106c168daf7645569579a15068fe2e5931
Rodrigo da Silva Pereira <rodrigosilvapereira7@gmail.com>
2026-07-13 03:13:56 -0300

    replay do histórico + apagar histórico + pequenos ajustes no design

--- main.js
+++ main.js
@@ -5,23 +5,47 @@ const tower_level_width = 90;
 const tower_level_height = 100;
 const tower_level_decrement = 8;
 
-for (let i = 0; i < levels; i++) {
-    const tower_level = document.createElement("img");
-    tower_level.classList.add("tower-level");
-    tower_level.src = "/sprites/tower-level.png";
-    tower_level.alt = "Tower Level";
+document.addEventListener("click", function(event){
+    const sidebar = document.getElementById("history-sidebar");
+    const clickedOutsideSidebar = !sidebar.contains(event.target);
 
-    tower_level.dataset.size = levels - i;
-    tower_level.style.width = `${tower_level_width - i * tower_level_decrement}px`;
+    if (!sidebar.classList.contains("hidden") && clickedOutsideSidebar){
+        document.getElementById("history-button").click();
+    }
+})
 
-    init_tower.appendChild(tower_level);
+function initializeGame() {
+    for (let i = 0; i < levels; i++) {
+        const tower_level = document.createElement("img");
+        tower_level.classList.add("tower-level");
+        tower_level.src = "/sprites/tower-level.png";
+        tower_level.alt = "Tower Level";
+
+        tower_level.dataset.size = levels - i;
+        tower_level.style.width = `${tower_level_width - i * tower_level_decrement}px`;
+
+        init_tower.appendChild(tower_level);
+    }
+    handleDraggableTowerLevel(document.getElementById("tower-1"));
 }
 
+initializeGame();
+
 function recordMove(fromTower, toTower, towerLevel) {
+    const parsedSize = parseInt(towerLevel.dataset.size);
+    const parsedFromTower = parseInt(fromTower.id.split("-")[1]);
+    const parsedToTower = parseInt(toTower.id.split("-")[1]);
+
     const historyList = document.getElementById("history-sidebar");
     const historyRecord = document.createElement("p");
     historyRecord.classList.add("history-record");
-    historyRecord.textContent = `Move-se o nível de tamanho ${towerLevel.dataset.size} da torre ${fromTower.id.split("-")[1]} para a torre ${toTower.id.split("-")[1]}`;
+
+    historyRecord.dataset.fromTower = parsedFromTower;
+    historyRecord.dataset.toTower = parsedToTower;
+    historyRecord.dataset.size = parsedSize;
+
+    historyRecord.textContent = `Move-se o nível de tamanho ${parsedSize} da torre ${parsedFromTower} para a torre ${parsedToTower}`;
+    
     historyList.appendChild(historyRecord);
     historyList.appendChild(document.createElement("hr"));
 
@@ -60,8 +84,6 @@ function handleDraggableTowerLevel(tower) {
     }
 }
 
-handleDraggableTowerLevel(document.getElementById("tower-1"));
-
 var dropZones = document.querySelectorAll(".drop-zone");
 dropZones.forEach(function (zone) {
     zone.addEventListener("dragover", function (e) {
@@ -93,9 +115,69 @@ dropZones.forEach(function (zone) {
 });
 
 const historyButton = document.getElementById("history-button");
-historyButton.addEventListener("click", function () {
+historyButton.addEventListener("click", function (event) {
     const historySidebar = document.getElementById("history-sidebar");
     historySidebar.classList.toggle("hidden");
     historyButton.classList.toggle("active");
+    event.stopPropagation();
 })
 
+function resetGame(cleanHistory = true) {
+    towers = document.querySelectorAll(".tower");
+    towers.forEach(tower => {
+        tower.replaceChildren();
+    });
+
+    if (cleanHistory){
+        lastHistoryListHeaderChild = document.getElementById("first-history-separator");
+        while (lastHistoryListHeaderChild.nextSibling) {
+            lastHistoryListHeaderChild.nextSibling.remove();
+        }
+
+        const moveCounter = document.getElementById("move-count-number");
+        moveCounter.textContent = 0;
+    }
+
+    initializeGame();
+}
+
+const resetHistoryButton = document.getElementById("reset-history-button");
+resetHistoryButton.addEventListener("click", resetGame);
+
+const replayButton = document.getElementById("replay-history-button");
+replayButton.addEventListener("click", function () {
+    const historyRecords = document.querySelectorAll(".history-record");
+
+    if (historyRecords.length == 0){
+        return;
+    }
+
+    resetGame(false);
+
+    document.getElementById("history-button").click();
+
+    const replayOverlay = document.getElementById("replay-overlay")
+    replayOverlay.classList.add("active");
+
+    historyRecords.forEach((record, index) => {
+        setTimeout(() => {
+            const fromTowerId = `tower-${record.dataset.fromTower}`;
+            const toTowerId = `tower-${record.dataset.toTower}`;
+
+            const fromTower = document.getElementById(fromTowerId);
+            const toTower = document.getElementById(toTowerId);
+            const towerLevel = fromTower.querySelector(`.tower-level[data-size="${record.dataset.size}"]`);
+
+            if (towerLevel) {
+                toTower.appendChild(towerLevel);
+                handleDraggableTowerLevel(fromTower);
+                handleDraggableTowerLevel(toTower);
+            }
+
+            if (index == historyRecords.length -1){
+                replayOverlay.classList.remove("active")
+            }
+        }, index * 1000)
+    });
+    
+});
